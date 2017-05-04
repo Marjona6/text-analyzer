03bef7a13e67a36646e025bfd7625cb37bd91cf0
Meryem Yanar <meryem.yanaryanar@gmail.com>
2017-05-04 13:17:04 +0300

    Fix several bugs, still a draft

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 "use strict";
 
-// var wordArray = [];
-
 // This function creates an array of individual words from the raw input text
 function getWordArray(userText) {
 // take the text input as a string
 // remove punctuation
 userText = userText.toLowerCase(); // can I do this this way? trying to make it all lowercase
-userText.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,""‌​);
-// push each separate string to an array
-// split it where there are spaces
+// userText.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,""‌​); // this line might have caused a problem? "Uncaught SyntaxError: Invalid or unexpected token" in console in chrome devtools
+// push each separate string to an array and split it where there are spaces
 var wordArray = userText.split(" ");
 // return the number of values in the array
 return wordArray;
@@ -59,3 +56,31 @@ function getAverageWordLength(userText) {
 	var averageWordLength = total / wordArray.length;
 	return averageWordLength;
 }
+
+// Function to handle Form Submit
+function handleFormSubmit() {
+	$('#js-input-text').submit(function(event) {
+// prevent default event of page reload/page redirect when submit is clicked
+	event.preventDefault();
+// empty results in case there are previous results? (How?)
+
+// take user input and assign it a JS variable name
+// remove hidden class from results section--does it matter if it comes before or after the calculations below?
+	$('dl').removeClass("hidden");
+	var userText = $(event.currentTarget).find('textarea[name="user-text"]').val();
+// do the functions required with the JS variable name as a parameter:
+// THESE ARE NOT APPEARING
+// place the result of word count function into the first dd
+	$('#dd_js-word-count-result').text(getWordCount(userText));
+	console.log(getWordCount(userText)); // this is working //
+// place the result of unique word count function into the second dd
+	$('#dd_js-unique-word-count-result').text(getUniqueWordCount(userText));
+// place the result of the average word length function into the third dd
+	$('#dd_js-average-word-length-result').text(getAverageWordLength(userText));
+	});
+}
+
+// Triggers go here
+$(function() {
+	handleFormSubmit();
+});
