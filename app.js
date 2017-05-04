ba84cc0e2a4bc6a899c08fbf363f99b32bbdbd05
Meryem Yanar <meryem.yanaryanar@gmail.com>
2017-05-04 17:13:54 +0300

    Finalize text analyzer

--- app.js
+++ app.js
@@ -3,35 +3,35 @@
 // This function creates an array of individual words from the raw input text
 function getWordArray(userText) {
 // take the text input as a string
+// make lowercase
+userText = userText.toLowerCase();
 // remove punctuation
-userText = userText.toLowerCase(); // can I do this this way? trying to make it all lowercase
-// userText.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,""‌​); // this line might have caused a problem? "Uncaught SyntaxError: Invalid or unexpected token" in console in chrome devtools
+var punctuationless = userText.replace(/[.,\/#!?$%\^&\*;:"@<>|{}=\-_`~()]/g,"");
+var finalString = punctuationless.replace(/\s{2,}/g," ");
 // push each separate string to an array and split it where there are spaces
-var wordArray = userText.split(" ");
-// return the number of values in the array
+var wordArray = finalString.split(" ");
+// return the array
 return wordArray;
 }
 
 // This function gets the total word count
 function getWordCount(userText) {
+// return the number of values in the array
 	return getWordArray(userText).length;
 }
 
 // Unique word count function
-// NOTE: This function needs to be corrected to remove case; current version will see Get and get as different words
-// ^ resolved? see function getWordArray above
 function getUniqueWordArray(userText) {
 // define an empty new array to hold the unique words
 	var uniqueWordArray = [];
 // use the array from the function above
 	var wordArray = getWordArray(userText);
-// put the first word of the old array into the first position of the new array
-	uniqueWordArray.push(wordArray[0]);
-// compare oldArray[i] to oldArray[i+1]; if they are different, then push the value of oldArray[i+1] into the new array
-// continue until the old array has been completely gone through
+// loop through the array
 	for (var i = 0; i < wordArray.length; i++) {
-		if (wordArray[i] !== wordArray[i+1]) {
-			uniqueWordArray.push(wordArray[i+1]);
+//if the current word is not already in the uniqueWordArray...
+		if (uniqueWordArray.indexOf(wordArray[i]) === -1) {
+// ...then we will add it to it
+            uniqueWordArray.push(wordArray[i]);
 		}
 	}
 // return the array of unique words
@@ -40,7 +40,8 @@ function getUniqueWordArray(userText) {
 
 // This function returns the length of the unique word count array (unique word count)
 function getUniqueWordCount(userText) {
-	return getUniqueWordArray.length;
+	var output = getUniqueWordArray(userText);
+	return output.length;	
 }
 
 // Average word length (in characters) function
@@ -62,21 +63,17 @@ function handleFormSubmit() {
 	$('#js-input-text').submit(function(event) {
 // prevent default event of page reload/page redirect when submit is clicked
 	event.preventDefault();
-// empty results in case there are previous results? (How?)
-
-// take user input and assign it a JS variable name
-// remove hidden class from results section--does it matter if it comes before or after the calculations below?
+// remove hidden class from results section
 	$('dl').removeClass("hidden");
+// take user input and assign it a JS variable name
 	var userText = $(event.currentTarget).find('textarea[name="user-text"]').val();
-// do the functions required with the JS variable name as a parameter:
-// THESE ARE NOT APPEARING
+// call the functions we need with the JS variable name as a parameter:
 // place the result of word count function into the first dd
-	$('#dd_js-word-count-result').text(getWordCount(userText));
-	console.log(getWordCount(userText)); // this is working //
+	$('#js-word-count-result').text(getWordCount(userText));
 // place the result of unique word count function into the second dd
-	$('#dd_js-unique-word-count-result').text(getUniqueWordCount(userText));
+	$('#js-unique-word-count-result').text(getUniqueWordCount(userText));
 // place the result of the average word length function into the third dd
-	$('#dd_js-average-word-length-result').text(getAverageWordLength(userText));
+	$('#js-average-word-length-result').text(getAverageWordLength(userText));
 	});
 }
 
